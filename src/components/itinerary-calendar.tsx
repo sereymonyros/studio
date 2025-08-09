2876b6113a9af191d5a66d65928d0af194e6a587
Sereymony Ros <sereymonyros@gmail.com>
2025-08-09 17:19:02 +0000

    https://travelnevada.com/wp-content/uploads/2015/06/hoover-dam-1.jpg for

--- src/components/itinerary-calendar.tsx
+++ src/components/itinerary-calendar.tsx
@@ -56,7 +56,7 @@ const ItineraryCalendar: FC<ItineraryCalendarProps> = ({ activities, onAddActivi
     [`${year}-08-16`]: "https://twoaztrains.com/wp-content/uploads/2022/12/Open-Air-Viewing-Cars-Verde-Canyon-Train-mb8.jpg",
     [`${year}-08-17`]: "https://themaritimeexplorer.ca/wp-content/uploads/2024/12/Walnut-Canyon-Dwellings-768x440.jpg",
     [`${year}-08-18`]: 'https://cdn.allgrandcanyon.com/images/content/19995_1323283002_H6b08_lg.jpg',
-    [`${year}-08-19`]: '/images/day5.jpg',
+    [`${year}-08-19`]: 'https://travelnevada.com/wp-content/uploads/2015/06/hoover-dam-1.jpg',
     [`${year}-08-20`]: '/images/day6.jpg',
     [`${year}-08-21`]: '/images/day7.jpg',
     [`${year}-08-22`]: '/images/day8.jpg',

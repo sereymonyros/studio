951e98250895d791985c94771769a2fcc382a291
Sereymony Ros <sereymonyros@gmail.com>
2025-08-09 17:24:46 +0000

    https://static.wixstatic.com/media/8de0c0_a38585f9e62d4672b5a0889001f317

--- src/components/itinerary-calendar.tsx
+++ src/components/itinerary-calendar.tsx
@@ -58,7 +58,7 @@ const ItineraryCalendar: FC<ItineraryCalendarProps> = ({ activities, onAddActivi
     [`${year}-08-18`]: 'https://cdn.allgrandcanyon.com/images/content/19995_1323283002_H6b08_lg.jpg',
     [`${year}-08-19`]: 'https://travelnevada.com/wp-content/uploads/2015/06/hoover-dam-1.jpg',
     [`${year}-08-20`]: 'https://www.traveloffpath.com/wp-content/uploads/2022/05/Las-Vegas-strip-at-night-.jpg',
-    [`${year}-08-21`]: '/images/day7.jpg',
+    [`${year}-08-21`]: 'https://static.wixstatic.com/media/8de0c0_a38585f9e62d4672b5a0889001f317cc~mv2.jpg/v1/fill/w_500,h_375,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/8de0c0_a38585f9e62d4672b5a0889001f317cc~mv2.jpg',
     [`${year}-08-22`]: '/images/day8.jpg',
     // Add more dates and URLs as needed
   };

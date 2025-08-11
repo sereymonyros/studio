26ad84644dcf9bcd67b9ab2763fc99467d0b75c2
Sereymony Ros <sereymonyros@gmail.com>
2025-08-11 20:38:56 +0000

    replace this https://firebasestorage.googleapis.com/v0/b/astral-web-4607

--- src/components/itinerary-calendar.tsx
+++ src/components/itinerary-calendar.tsx
@@ -54,7 +54,7 @@ const ItineraryCalendar: FC<ItineraryCalendarProps> = ({ activities, onAddActivi
   const dayBackgroundImages: Record<string, string> = {
     [`${year}-08-15`]: "https://firebasestorage.googleapis.com/v0/b/astral-web-460708-r4.firebasestorage.app/o/sedona.png?alt=media&token=b9789354-8b22-4034-a657-61683780da60",
     [`${year}-08-16`]: "https://firebasestorage.googleapis.com/v0/b/astral-web-460708-r4.firebasestorage.app/o/08-16.png?alt=media&token=12547b94-eb4d-4fe8-9c10-e34e6f457f25",
-    [`${year}-08-17`]: "https://themaritimeexplorer.ca/wp-content/uploads/2024/12/Walnut-Canyon-Dwellings-768x440.jpg",
+    [`${year}-08-17`]: "https://firebasestorage.googleapis.com/v0/b/astral-web-460708-r4.firebasestorage.app/o/08-17.png?alt=media&token=f3809b3f-af67-49de-aab3-e647c2e1aad1",
     [`${year}-08-18`]: 'https://firebasestorage.googleapis.com/v0/b/astral-web-460708-r4.firebasestorage.app/o/08-18.png?alt=media&token=51d72d6b-b709-4b7e-b0a2-dd8c08681892',
     [`${year}-08-19`]: 'https://travelnevada.com/wp-content/uploads/2015/06/hoover-dam-1.jpg',
     [`${year}-08-20`]: 'https://www.traveloffpath.com/wp-content/uploads/2022/05/Las-Vegas-strip-at-night-.jpg',

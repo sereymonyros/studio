3f46945cb108807957deb9d0e3e995d7a51b51d2
Sereymony Ros <sereymonyros@gmail.com>
2025-08-11 18:05:41 +0000

    make it opacity 50%

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -125,25 +125,27 @@ function ItineraryPage() {
         </div>
       </header>
       <main className="flex-grow container mx-auto p-4 md:p-8">
-        <div className="flex justify-center">
-          <div className="grid lg:grid-cols-5 gap-8 items-start w-full max-w-6xl">
-            <div className="lg:col-span-3 flex flex-col gap-8">
-              {isLoadingActivities ? (
-                  <p>{t.calendar.loading}</p>
-              ) : (
-                  <ItineraryCalendar 
-                    activities={activities}
-                    onAddActivity={handleAddActivity}
-                    onUpdateActivity={handleUpdateActivity}
-                    onDeleteActivity={handleDeleteActivity}
-                    isReadOnly={false}
-                    lang={lang}
-                    t={t}
-                  />
-              )}
-            </div>
-            <div className="lg:col-span-2">
-              <AiSuggestions suggestions={suggestions} isLoading={isLoadingSuggestions} t={t.suggestions} />
+        <div className="bg-black/50 rounded-lg p-4">
+          <div className="flex justify-center">
+            <div className="grid lg:grid-cols-5 gap-8 items-start w-full max-w-6xl">
+              <div className="lg:col-span-3 flex flex-col gap-8">
+                {isLoadingActivities ? (
+                    <p>{t.calendar.loading}</p>
+                ) : (
+                    <ItineraryCalendar 
+                      activities={activities}
+                      onAddActivity={handleAddActivity}
+                      onUpdateActivity={handleUpdateActivity}
+                      onDeleteActivity={handleDeleteActivity}
+                      isReadOnly={false}
+                      lang={lang}
+                      t={t}
+                    />
+                )}
+              </div>
+              <div className="lg:col-span-2">
+                <AiSuggestions suggestions={suggestions} isLoading={isLoadingSuggestions} t={t.suggestions} />
+              </div>
             </div>
           </div>
         </div>

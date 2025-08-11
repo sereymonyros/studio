21c3b8eaee63fa0f1f722fd36a4e9b67ff5d1796
Sereymony Ros <sereymonyros@gmail.com>
2025-08-11 22:38:00 +0000

    in app header, ceter div: make title, and action displayed as stack

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -111,9 +111,9 @@ function ItineraryPage() {
   return (
     <div className="flex flex-col min-h-screen">
       <header className="bg-primary/80 text-white-foreground py-6 px-4 md:px-8 shadow-md">
-        <div className="container mx-auto flex justify-between items-center">
-          <div>
-            <h1 className="text-3xl md:text-4xl font-bold font-headline flex items-center gap-3">
+        <div className="container mx-auto flex flex-col items-center gap-4">
+          <div className="text-center">
+            <h1 className="text-3xl md:text-4xl font-bold font-headline flex items-center justify-center gap-3">
               <Sunrise className="w-8 h-8"/>
               {t.header.title}
             </h1>

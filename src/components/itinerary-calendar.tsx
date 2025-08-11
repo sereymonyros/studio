e09a6836c47f764cff04f9c3b199d49ca320acff
Sereymony Ros <sereymonyros@gmail.com>
2025-08-11 19:00:11 +0000

    https://firebasestorage.googleapis.com/v0/b/astral-web-460708-r4.firebas

--- src/components/itinerary-calendar.tsx
+++ src/components/itinerary-calendar.tsx
@@ -52,7 +52,7 @@ const ItineraryCalendar: FC<ItineraryCalendarProps> = ({ activities, onAddActivi
   };
 
   const dayBackgroundImages: Record<string, string> = {
-    [`${year}-08-15`]: "https://chasingexperiencesvlog.com/wp-content/uploads/2024/07/Arizona-1.png", // Replace with your actual image URLs
+    [`${year}-08-15`]: "https://firebasestorage.googleapis.com/v0/b/astral-web-460708-r4.firebasestorage.app/o/sedona.png?alt=media&token=b9789354-8b22-4034-a657-61683780da60", // Replace with your actual image URLs
     [`${year}-08-16`]: "https://twoaztrains.com/wp-content/uploads/2022/12/Open-Air-Viewing-Cars-Verde-Canyon-Train-mb8.jpg",
     [`${year}-08-17`]: "https://themaritimeexplorer.ca/wp-content/uploads/2024/12/Walnut-Canyon-Dwellings-768x440.jpg",
     [`${year}-08-18`]: 'https://cdn.allgrandcanyon.com/images/content/19995_1323283002_H6b08_lg.jpg',

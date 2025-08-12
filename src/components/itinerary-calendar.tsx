62090873bd21ddcb965f9dbf84808218e02d5f18
Sereymony Ros <sereymonyros@gmail.com>
2025-08-12 17:07:28 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <FlightInfo title=

--- src/components/itinerary-calendar.tsx
+++ src/components/itinerary-calendar.tsx
@@ -105,7 +105,7 @@ const ItineraryCalendar: FC<ItineraryCalendarProps> = ({ activities, onAddActivi
         {passengers.map(p => (
           <div key={p.name} className="flex items-center gap-1.5">
               <User className="w-3 h-3"/>
-              <span className="font-semibold text-white/70">{p.name}:</span>
+              <span className="font-semibold text-white">{p.name}:</span>
               <span className="text-white/70">{p.seat}</span>
           </div>
         ))}

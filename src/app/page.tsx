8aaa5dca296961724e67b8def84887d61bbaf2de
Sereymony Ros <sereymonyros@gmail.com>
2025-08-12 21:05:59 +0000

    hide edit and delete buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ function ItineraryPage() {
   const [isLoadingSuggestions, setIsLoadingSuggestions] = useState(false);
   const [isLoadingActivities, setIsLoadingActivities] = useState(true);
   const [lang, setLang] = useState<Language>('en');
-  const [isAdmin, setIsAdmin] = useState(true);
+  const [isAdmin, setIsAdmin] = useState(false);
   const { toast } = useToast();
   
   const t = translations[lang];

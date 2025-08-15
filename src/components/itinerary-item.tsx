0ece6a41e5a70ac0024c6d119d32eb2fdbfe88ec
Sereymony Ros <sereymonyros@gmail.com>
2025-08-15 00:35:13 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <CardContent class

--- src/components/itinerary-item.tsx
+++ src/components/itinerary-item.tsx
@@ -93,7 +93,7 @@ export default function ItineraryItem({ activity, onUpdateActivity, onDeleteActi
           <DialogTrigger asChild>
              <div className="cursor-pointer" onClick={() => setDetailViewOpen(true)}>
                 <Card className={cn(
-                  "transition-all hover:shadow-md bg-card/60 border-0 rounded-3xl",
+                  "transition-all hover:shadow-md bg-card/60 border-0 rounded-[35px]",
                   "hover:bg-card/100 focus:bg-card/100 border-white"
                    
                 )}>
